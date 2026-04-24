8c7b72558178c4579b0bc25a201c89a3038fd703
unknown <dyddl789@gmail.com>
2026-04-24 16:57:09 +0900

    test4

--- backend/ess_monitoring/src/main/java/com/lgy/ess_monitoring/service/EssMonitoringService.java
+++
@@ -1,9 +0,0 @@
-package com.lgy.ess_monitoring.service;
-
-import java.util.ArrayList;
-
-import com.lgy.ess_monitoring.dto.EssMonitoringDTO;
-
-public interface EssMonitoringService {
-    public ArrayList<EssMonitoringDTO> getData(int memberId);
-}
